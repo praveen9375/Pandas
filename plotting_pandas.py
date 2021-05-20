895d9afeee7aa16ec45085dbb31659bc6a613193
praveen9375 <48412124+praveen9375@users.noreply.github.com>
2021-05-20 21:13:43 +0545

    Create plotting_pandas.py

---
+++ plotting_pandas.py
@@ -0,0 +1,7 @@
+"""Basics
+The main function used for plotting DataFrames is plot. This function is used in tandem with the show function from the pyplot API, to produce plot visualizations. 
+We import the pyplot API with the line: import matplotlib.pyplot as plt"""
+
+print('{}\n'.format(df))
+df.plot()
+plt.show()
